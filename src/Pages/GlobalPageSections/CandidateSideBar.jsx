83a237c8b9fda0c2703812e6bf6b3a83051e4398
Basit <odoobasit@gmail.com>
2024-06-04 09:44:22 -0700

    CandidateSideBar

--- src/Pages/GlobalPageSections/CandidateSideBar.jsx
+++ src/Pages/GlobalPageSections/CandidateSideBar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const CandidateSideBar = () => {
-  return <div className="">CandidateSideBar</div>;
+  return (
+    <div className="side-bar-main-div">
+      <h2 className="side-bar-first-heading">Recent Activity</h2>
+
+      <div className="side-bar-first-box">
+        <div className="columns">
+          <div className="side-bar-fc">
+            <h2 className="candidate-text">Smash My Trash</h2>
+            <h4 className="candidate-text">Nick Hart</h4>
+            <h4 className="candidate-text">Fresno CA 93711</h4>
+            <p className="candidate-text">05/30/2024 10:01 am</p>
+          </div>
+          <div className="side-bar-sc">
+            <button className="side-bar-btn">Pending</button>
+            <h4 className="candidate-text">David Curnich</h4>
+            <h4 className="candidate-text">317-601-7247</h4>
+            <div className="Message">
+              <h4 className="candidate-text">MESSAGE</h4>
+              <h4 className="candidate-text h">|</h4>
+              <h4 className="candidate-text">SEND RE-CHECK</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CandidateSideBar;
