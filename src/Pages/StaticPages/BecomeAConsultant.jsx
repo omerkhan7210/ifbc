a851b258a3feb7066a60218d725ed2895f8ec331
Baqar Abbas <baqar.abbas@hotmail.com>
2024-08-06 12:00:29 -0700

    Video height and width adjusted as per the requirements shared by Kamran bhai - content of Book a consultant tab will be updated soon

--- src/Pages/StaticPages/BecomeAConsultant.jsx
+++ src/Pages/StaticPages/BecomeAConsultant.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 const BecomeAConsultant = () => {
   return (
-    <div>
+    <div className='flex justify-center'>
       <video 
       src='/video/becomeaconsultant.mp4' 
       type="video/mp4"
-      autoPlay 
-      controls 
+      width="60%" 
+      height="60%"
+      autoPlay
+      controls
       loop
-      className='w-full h-full object-cover'
+      // className='w-full h-full object-cover'
+      className='rounded-3xl'
       >
       </video>
     </div>
